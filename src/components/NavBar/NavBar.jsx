996f151f56c82a65c4826a6677bd0c5473045c26
23carnies <46233990+23carnies@users.noreply.github.com>
2020-10-18 18:33:36 -0400

    Add language flags to navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,6 +10,10 @@ const NavBar = () => {
                     <li className="item" id="left"><a href="#">About Me</a></li>
                     <li className="item"><a href="#">Projects</a></li>
                     <li className="item"><a href="#">Contact</a></li>
+                    <div className="flag-box">
+                        <li className="flag"><a href="#"><span class="flag-icon flag-icon-us"></span></a></li>
+                        <li className="flag"><a href="#"><span class="flag-icon flag-icon-es"></span></a></li>
+                    </div>
                 </ul>
             </nav>
         </header>
